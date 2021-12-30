6aac0a8e35574fd8db7d5e9c688bec2713fc7aa8
sadafamininia99 <aminicomputer96@gmail.com>
2021-12-30 22:25:30 +0330

    setinterval

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-//grab the dom we need :)) 
-
 const days = document.getElementById("days");
 const hours = document.getElementById("hours");
 const minutes = document.getElementById("minutes");
@@ -12,34 +10,31 @@ const currentYear = new Date().getFullYear();
 
 const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
 
-
-// 
 // Set background year
-// year.innerText = currentYear + 1;
-// 
+year.innerText = currentYear + 1;
+
 // Update countdown time
-// function updateCountdown() {
-//   const currentTime = new Date();
-//   const diff = newYearTime - currentTime;
-// 
-//   const d = Math.floor(diff / 1000 / 60 / 60 / 24);
-//   const h = Math.floor(diff / 1000 / 60 / 60) % 24;
-//   const m = Math.floor(diff / 1000 / 60) % 60;
-//   const s = Math.floor(diff / 1000) % 60;
-// 
-//   Add values to DOM
-//   days.innerHTML = d;
-//   hours.innerHTML = h < 10 ? "0" + h : h;
-//   minutes.innerHTML = m < 10 ? "0" + m : m;
-//   seconds.innerHTML = s < 10 ? "0" + s : s;
-// }
-// 
+function updateCountdown() {
+  const currentTime = new Date();
+  const diff = newYearTime - currentTime;
+
+  const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+  const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+  const m = Math.floor(diff / 1000 / 60) % 60;
+  const s = Math.floor(diff / 1000) % 60;
+
+  // Add values to DOM
+  days.innerHTML = d;
+  hours.innerHTML = h < 10 ? "0" + h : h;
+  minutes.innerHTML = m < 10 ? "0" + m : m;
+  seconds.innerHTML = s < 10 ? "0" + s : s;
+}
+
 // Show spinner before countdown
-// setTimeout(() => {
-//   loading.remove();
-//   countdown.style.display = "flex";
-// }, 1000);
-// 
+setTimeout(() => {
+  loading.remove();
+  countdown.style.display = "flex";
+}, 1000);
+
 // Run every second
-// setInterval(updateCountdown, 1000);
-// 
+setInterval(updateCountdown, 1000);
